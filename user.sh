c5b92fd3c90c3f07be16eaa26050b82c380d3f70
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-08 14:01:15 +0530

    doing change in user .sh

--- user.sh
+++ user.sh
@@ -1,8 +1,9 @@
 cp user.service /etc/systemd/system/user.service
-useradd roboshop
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
+useradd roboshop
+
 rm -rf /app
 mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
